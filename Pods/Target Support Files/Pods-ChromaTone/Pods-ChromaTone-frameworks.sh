bf1777bff3d48dd59c10e433a0c54c2830fd5548
newinh <newinh90@gmail.com>
2017-02-25 02:57:22 +0900

    install fontawesome

--- Pods/Target Support Files/Pods-ChromaTone/Pods-ChromaTone-frameworks.sh
+++ Pods/Target Support Files/Pods-ChromaTone/Pods-ChromaTone-frameworks.sh
@@ -90,9 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/AudioKit/iOS/AudioKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FontAwesome.swift/FontAwesome_swift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/AudioKit/iOS/AudioKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FontAwesome.swift/FontAwesome_swift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
